1dc6f64e8a31976b56d6cc59e10082f084da64f0
m-sabooh <m.sabooh@outlook.com>
2023-08-23 06:19:58 +0300

    maybe final

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,80 @@
+import {Component, OnInit} from '@angular/core';
+import {Cart, CartItem} from "../../models/cart.model";
+import {CartService} from "../../services/cart.service";
+import {HttpClient} from "@angular/common/http";
+import {async} from "rxjs";
+import {loadStripe} from "@stripe/stripe-js";
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 150,
+        quantity: 2,
+        id: 1
+      },
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 150,
+        quantity: 1,
+        id: 1
+      }]
+  };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ]
+
+  constructor(private cartService: CartService, private http: HttpClient) {}
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart: Cart)=> {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any)=>{
+      let stripe = await loadStripe('pk_test_51NhjIPHLDQAdNEJqq1aIdC91FohIgUtLBqa5Qk5AKR7FpmFjXM0a2AieqUKEKd8tFj4xoUU3wa448W0ytRBF4Ba000THzv6Mzt');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    });
+  }
+}
